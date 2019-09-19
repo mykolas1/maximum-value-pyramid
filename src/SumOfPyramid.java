964fc0a1f424f69139a0a3da0ed53708bca3a3f1
mykolas1 <mykolasba9@gmail.com>
2019-09-19 19:52:12 +0300

    Algorithm's bugs correction.

--- src/SumOfPyramid.java
+++ src/SumOfPyramid.java
@@ -1,38 +1,24 @@
+import resource.TestCases;
+
 import java.io.*;
+import java.util.ResourceBundle;
 
 public class SumOfPyramid {
-
-    private static final String WRONG_INPUT = "Input array does not have a path to down layer.";
-
-    private static final int[][] input = new int[][]{
-            { 215, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 192, 124, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 117, 269, 442, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 218, 836, 347, 235, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 320, 805, 522, 417, 345, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 229, 601, 728, 835, 133, 124, 0, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 248, 202, 277, 433, 207, 263, 257, 0, 0, 0, 0, 0, 0, 0, 0},
-            { 359, 464, 504, 528, 516, 716, 871, 182, 0, 0, 0, 0, 0, 0, 0},
-            { 461, 441, 426, 656, 863, 560, 380, 171, 923, 0, 0, 0, 0, 0, 0},
-            { 381, 348, 573, 533, 448, 632, 387, 176, 975, 449, 0, 0, 0, 0, 0},
-            { 223, 711, 445, 645, 245, 543, 931, 532, 937, 541, 444, 0, 0, 0, 0},
-            { 330, 131, 333, 928, 376, 733,  17, 778, 839, 168, 197, 197, 0, 0, 0},
-            { 131, 171, 522, 137, 217, 224, 291, 413, 528, 520, 227, 229, 928, 0, 0},
-            { 223, 626,  34, 683, 839,  52, 627, 310, 713, 999, 629, 817, 410, 121, 0},
-            { 924, 622, 911, 233, 325, 139, 721, 218, 253, 223, 107, 233, 230, 124, 233}
-    };
 
     static class Result {
         int sumValue;
         boolean evenFlag;
     }
     public static void main(String[] args) throws FileNotFoundException {
-
+        int[][] input = TestCases.input;
+        ResourceBundle bundle = ResourceBundle.getBundle("resource/config");
+        validatePyramid(bundle, input);
+        System.out.println("The assumption is made that 0 (ZERO) is even number.");
         int sumOfPath = findMaximumSum(input);
         if (sumOfPath == 0) {
-            System.out.println(WRONG_INPUT);
+            System.out.println(bundle.getString("WRONG_INPUT"));
         } else
-            System.out.println(sumOfPath);
+            System.out.println("Result:" + sumOfPath);
     }
     private static int findMaximumSum(int[][] a) {
         int sum = 0;
@@ -42,32 +28,41 @@ public class SumOfPyramid {
         return result.sumValue;
     }
 
-    private static void findSumLogic(int[][] a, int i, int j, int sum, Result result) {
-        if (i >= a.length) {
+    private static void findSumLogic(int[][] array, int i, int j, int sum, Result result) {
+        if (i >= array.length) {
             changeResultSumIfLess(result, sum);
+            changeFlag(result);
             return;
         }
-        int origSum = addSumToNode(sum, a[i][j]);
+        int origSum = addSumToNode(sum, array[i][j]);
 
-        if (i < a.length - 1) {
-            if (isEven(input[i + 1][j]) == result.evenFlag && isEven(input[i + 1][j + 1]) == result.evenFlag) {
+        if (i < array.length - 1) {
+            if (isEven(array[i + 1][j]) == result.evenFlag && isEven(array[i + 1][j + 1]) == result.evenFlag) {
                 changeFlag(result);
-                findSumLogic(a, i + 1, j, origSum, result);
+                findSumLogic(array, i + 1, j, origSum, result);
                 changeFlag(result);
-                findSumLogic(a, i + 1, j + 1, origSum, result);
+                findSumLogic(array, i + 1, j + 1, origSum, result);
+            } else if (isEven(array[i + 1][j]) == result.evenFlag && isEven(array[i + 1][j + 1]) != result.evenFlag) {
                 changeFlag(result);
-            } else if (isEven(input[i + 1][j]) == result.evenFlag && isEven(input[i + 1][j + 1]) != result.evenFlag) {
+                findSumLogic(array, i + 1, j, origSum, result);
+            } else if (isEven(array[i + 1][j]) != result.evenFlag && isEven(array[i + 1][j + 1]) == result.evenFlag) {
                 changeFlag(result);
-                findSumLogic(a, i + 1, j, origSum, result);
-            } else if (isEven(input[i + 1][j]) != result.evenFlag && isEven(input[i + 1][j + 1]) == result.evenFlag) {
-                changeFlag(result);
-                findSumLogic(a, i + 1, j + 1, origSum, result);
+                findSumLogic(array, i + 1, j + 1, origSum, result);
             }
+                changeFlag(result);
         }
 
         //change result.sumValue value if it is less then origSum on down layer of pyramid.
-        if (i == a.length-1) {
+        if (i == array.length-1) {
             changeResultSumIfLess(result, origSum);
+            changeFlag(result);
+        }
+    }
+
+    private static void validatePyramid(ResourceBundle bundle, int[][] input) {
+        if (isEven(input[0][0])) {
+            System.out.println(bundle.getString("WRONG_INPUT"));
+            System.exit(1);
         }
     }
 
